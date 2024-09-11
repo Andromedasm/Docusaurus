b7bb8ceaa5636ea758788fd8b6b29305daeaa206
Asmeg <repulsive7@gmail.com>
2024-09-11 15:47:15 +0900

    20240911

--- manual-website/sidebars.ts
+++ manual-website/sidebars.ts
@@ -111,6 +111,7 @@ const sidebars: SidebarsConfig = {
                 'linux/git',
                 'linux/ubuntu-guide',
                 'linux/rhel-guide',
+                'linux/azure-fsck',
             ],
         },
         {
